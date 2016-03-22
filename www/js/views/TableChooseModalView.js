852398f35e86da5559a15a8cc176846243625850
Hugo Gonzalez <pinelo93@gmail.com>
2016-03-21 23:17:26 -0600

    Adds reservation acceptance functionality.

---
+++ www/js/views/TableChooseModalView.js
@@ -0,0 +1,24 @@
+var TableChooseModalView = function () {
+	 
+	 var tables;
+
+	 this.initialize = function () {
+	 	 this.$el = $('<div/>') 
+	 	 this.render();
+	 } 
+
+	 this.setTables = function (list) {
+	 	 tables = list;
+	 	 this.render();
+	 }
+
+	 this.render = function () {
+	 	 this.$el.html(this.template(tables));
+		 	$('.collapsible').collapsible({
+		      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		    });
+	 	 return this ;
+	 }
+
+	 this.initialize();
+}
